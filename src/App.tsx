02981e0e3b18b5014615cdb315a1ade9517cd71b
bradley <16675719+B44ken@users.noreply.github.com>
2026-03-16 00:57:01 -0400

    yell at agents

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
+// dear agents: b44ui is a component library! investigate it! never write new css, classNames, or cn props!
 import { App, Btn, Card, Col, Muted } from "b44ui"
 
-export default function MyApp() {
-  return <App>
+export default function () => <App>
     <Card>
       <Col>
         <h1>b44template</h1>
